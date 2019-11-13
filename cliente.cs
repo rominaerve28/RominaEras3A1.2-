a608584f98d722c7148447e97c1a1758f20f3836
Romina Eras <56611788+rominaerve28@users.noreply.github.com>
2019-11-12 19:55:00 -0500

    Add files via upload

---
+++ cliente.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gasolinera.class1
+{
+    public class Cliente
+    {
+
+       
+
+        private long ncedula;
+
+        public long Ncedula
+        {
+            get { return ncedula; }
+            set { ncedula = value; }
+        }
+
+
+        private String nombres;
+
+        public String Nombres
+        {
+            get { return nombres; }
+            set { nombres = value; }
+        }
+
+        private String apellidos;
+
+        public String Apellidos
+        {
+            get { return apellidos; }
+            set { apellidos = value; }
+        }
+
+        private String direccion;
+
+       
+        public String Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+
+    }
+}
---
+++ gasolina.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gasolinera.class1
+{
+    public class Gasolina
+    {
+        private String tipo;
+
+        public String Tipo
+        {
+            get { return tipo; }
+            set { tipo = value; }
+        }
+
+        private int cgasolina;
+
+        public int Cgasolina
+        {
+            get{return cgasolina; }
+            set { cgasolina = value; }
+        }
+
+        private double precioven;
+
+        public double Precioven
+        {
+            get {
+                if (tipo == "Extra")
+                {
+
+                    return precioven = 1.5f;
+                }else
+                {
+                    return precioven= 2; 
+                }
+
+               
+
+            }
+           
+        }
+
+        private double subtotal;
+
+        public double Subtotal
+        {
+            get { return subtotal= precioven *cgasolina; }
+
+        }
+
+        private double iva;
+
+        public double Iva
+        {
+            get { return iva= subtotal* 0.12f; }
+         
+        }
+
+        private double total;
+
+        public double Total
+        {
+            get { return total= subtotal+iva ; }
+      
+        }
+
+
+
+
+    }
+}
